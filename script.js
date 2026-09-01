da111ae9209f0030e019736bf179bac88024e3bc
gimonteiro11 <giovannajesusmonteiro@gmail.com>
2026-09-01 09:02:53 -0300

    ok

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentloaded", () => {
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.queryselector(".post-media");
+if (!likeBtn) return;
+
+    const likesCountSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
